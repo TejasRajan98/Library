67d1a1aeadcabf94c0cdf29d0de5877285d592bc
Tejas Rajan <rajan.tejas@yahoo.com>
2022-08-16 12:09:51 +0530

    setter

--- src/data/Book.java
+++ src/data/Book.java
@@ -1,9 +1,24 @@
 package data;
 
 public class Book {
-    String title;
-    String yearOfPublication;
-    long ISBN;
-    String author;
-    double price;
+    private String title;
+    private String yearOfPublication;
+    private long ISBN;
+    private String author;
+    private double price;
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setYearOfPublication(String yearOfPublication){
+        this.yearOfPublication = yearOfPublication;
+    }
+    public void setISBN(long ISBN){
+        this.ISBN = ISBN;
+    }
+    public void setAuthor(String author){
+            this.author = author;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
 }
